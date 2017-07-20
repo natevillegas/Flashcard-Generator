337175a2bb79f0733b10165c69bd3ccfd854af9b
Nate Villegas <natemv@gmail.com>
2017-07-19 21:04:49 -0700

    almost there

---
+++ ClozeCard.js
@@ -0,0 +1,14 @@
+var BasicCard = require ("./BasicCard");
+
+var CloseCard = function (text, cloze) {
+	this.text = text;
+	this.cloze = cloze;
+	this.fullText = console.log(this.text);
+	this.partial = function () {
+		var newText = 
+		else
+			console.log()
+	};
+};
+
+module.exports = ClozeCard;
